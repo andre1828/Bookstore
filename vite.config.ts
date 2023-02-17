06bd74b816c615c39a475d0430510117d2cd6ee3
andre1828 <andremelo13@gmail.com>
2023-02-17 18:16:00 -0300

    conf: install styled-components & VitePluginFonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
+import { VitePluginFonts } from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vite-react-ts-ghactions-template/',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePluginFonts({
+      google: {
+        families: ['Inter']
+      }
+    })
+  ],
   build: {
     sourcemap: true
   },
