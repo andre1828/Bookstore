b6c6bc6f92aa90508aedbb8b0bf8c02201f2b31e
andre1828 <andremelo13@gmail.com>
2023-02-19 00:48:17 -0300

    feat: add bookcase component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,22 +4,41 @@ import defaultTheme from '../theme/default-theme';
 import Book from '../book';
 import cover from '../book/cover2.png';
 import Shelf from '../Shelf';
+import Bookcase from '../bookcase';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Shelf>
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-        <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" />
-      </Shelf>
+      <Bookcase>
+        <Shelf title="Aventura">
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+        </Shelf>
+        <Shelf title="Ação">
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+        </Shelf>
+        <Shelf isHighlighted title="Destaques">
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+          <Book cover={cover} title="O menino Nelson Mandela" author="Viviana Mazza" isCoverOnly />
+        </Shelf>
+      </Bookcase>
     </ThemeProvider>
   );
 }
---
+++ src/bookcase/Bookcase.tsx
@@ -0,0 +1,9 @@
+interface BookCaseProps {
+  children: JSX.Element;
+}
+
+const Bookcase = ({ children }: BookCaseProps) => {
+  return <main>{children}</main>;
+};
+
+export default Bookcase;
